df5180431fb0e4b3a1d82c8bad60f748dd86f134
Ian A.B. King <iansens@hotmail.com>
2018-09-17 18:54:28 +0200

    add working directory clean script

--- build.bat
+++
@@ -1,4 +0,0 @@
-md build
-cd build
-cmake ..
-cmake --build .
---
+++ s_clean_workdir.sh
@@ -0,0 +1,6 @@
+read -p "`echo $'\n.'` THIS WILL RESET THE WORKING DIRECTORY TO THE LATEST COMMIT `echo $'\n.'` CONTINUE? (Y/N) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    git clean -d -x -f
+fi
