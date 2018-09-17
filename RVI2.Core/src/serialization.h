60749133442b21a0876e69993a59d844128528f4
Ian A.B. King <iansens@hotmail.com>
2018-09-17 18:54:18 +0200

    wip

--- RVI2.Core/src/serialization.h
+++ RVI2.Core/src/serialization.h
@@ -13,11 +13,20 @@ namespace rvi
     FWD_DECL_CLASS(ColorRGBA);
     FWD_DECL_CLASS(Vertex);
 
+    struct SerializedStringInfo
+    {
+        size_t CharacterSize;
+        size_t StringLength;
+    };
+
     //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
     // Serializer class
     //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
     class Serializer
     {
+        //---------------------------------------
+        // Serialization
+        //---------------------------------------
     public:
         template<typename T, TEMPLATE_ENABLE_IF_IS_INTEGER(T)>
         static size_t SerializeInteger(std::vector<U8>& data_container, const T& val)
@@ -43,6 +52,9 @@ namespace rvi
         template<typename T, TEMPLATE_ENABLE_IF_IS_POD(T)>
         static size_t SerializePOD(std::vector<U8>& data_container, const T& pod_obj)
         {
+            // Resize container to fit new data
+            data_container.reserve(data_container.size() + sizeof(T));
+
             U8* pod_obj_ptr = (U8*)&pod_obj;
             for (size_t i = 0; i < sizeof(pod_obj); ++i)
             {
@@ -59,6 +71,9 @@ namespace rvi
 
             // Get string character size
             U8 char_sz = (U8)(sizeof(str.value_type()));
+
+            // Reserve buffer size in target container
+            data_container.reserve(data_container.size() + buff_sz);
 
             // Push buffer size and character size to the container
             Serializer::SerializeInteger<U8>(data_container, buff_sz);
@@ -81,6 +96,63 @@ namespace rvi
                 }
             }
             return buff_sz;
+        }
+
+        //---------------------------------------
+        // Deserialization
+        //---------------------------------------
+    public:
+
+        static std::string DeserializeString8(const std::vector<U8>& data_container, size_t offset)
+        {
+            return DeserializeString<std::string>(data_container, offset);
+        }
+
+    private:
+        template<typename T, TEMPLATE_ENABLE_IF_IS_POD(T)>
+        static T DeserializePOD(const std::vector<U8>& data_container, size_t offset)
+        {
+            size_t obj_sz = sizeof(T);
+            assert((offset + obj_sz) <= data_container.size());
+            T result;
+            std::memcpy(&result, (data_container.data() + offset), obj_sz);
+            return result;
+        }
+
+        template<typename T, std::enable_if<std::is_base_of<std::basic_string, T>::value>::type>
+        static T DeserializeString(const std::vector<U8>& data_container, size_t offset)
+        {
+            size_t currentOffset = 0;
+
+            U16 buffLen = DeserializePOD<U16>(data_container, currentOffset);
+            currentOffset += sizeof(U16);
+            U8 charSz = data_container[currentOffset];
+            currentOffset += sizeof(U8);
+
+            U16 strLen = ((buffLen / charSz) - sizeof(U8));
+
+            T result;
+            result.reserve(strlen * charSz);
+
+            size_t type_char_sz = sizeof(result.value_type());
+            assert(type_char_sz == charSz);
+
+            if (charSz == 1)
+            {
+                auto begin_it = (data_container.begin() + offset);
+                auto end_it = (data_container.begin() + offset + buffLen);
+                std::copy(begin_it, end_it, result.begin());
+            }
+            else
+            {
+                for (size_t i; i < strLen; i++)
+                {
+                    result.value_type auxChar = DeserializePOD(data_container, currentOffset + (i * sizeof(result.value_type)));
+                    result += auxChar;
+                }
+            }
+
+            return result;
         }
     };
 }
