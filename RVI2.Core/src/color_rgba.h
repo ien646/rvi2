3fd04283edba14efbd9394f09df2deb911da2c95
Ian A.B. King <iansens@hotmail.com>
2018-09-17 18:54:14 +0200

    Default colors

--- RVI2.Core/src/color_rgba.h
+++ RVI2.Core/src/color_rgba.h
@@ -4,6 +4,9 @@
 
 #include "rvi_base.h"
 
+#define CRGBA_STANDARD_COLOR(name,r,g,b,a)    \
+    static constexpr ColorRGBA name() { return ColorRGBA(r, g, b, a); }
+
 namespace rvi
 {
     class ColorRGBA
@@ -15,6 +18,7 @@ namespace rvi
         U8 A = 0;
 
         constexpr ColorRGBA() noexcept { };
+
         constexpr ColorRGBA(U8 r, U8 g, U8 b, U8 a) noexcept
         {
             R = r;
@@ -22,5 +26,24 @@ namespace rvi
             B = b;
             A = a;
         }
+
+        CRGBA_STANDARD_COLOR(TRANSPARENT, 0, 0, 0, 255);
+        CRGBA_STANDARD_COLOR(WHITE, 255, 255, 255, 0);
+        CRGBA_STANDARD_COLOR(BLACK, 0, 0, 0, 0);
+        CRGBA_STANDARD_COLOR(GREY, 127, 127, 127, 0);
+        CRGBA_STANDARD_COLOR(RED, 255, 0, 0, 0);
+        CRGBA_STANDARD_COLOR(RED_LIGHT, 255, 127, 127, 0);
+        CRGBA_STANDARD_COLOR(GREEN,0, 255, 0, 0);
+        CRGBA_STANDARD_COLOR(GREEN_LIGHT, 127, 255, 127, 0);
+        CRGBA_STANDARD_COLOR(BLUE, 0, 0, 255, 0);
+        CRGBA_STANDARD_COLOR(BLUE_LIGHT, 127, 127, 255, 0);
+        CRGBA_STANDARD_COLOR(YELLOW, 255, 255, 0, 0);
+        CRGBA_STANDARD_COLOR(YELLOW_LIGHT, 255, 255, 127, 0);
+        CRGBA_STANDARD_COLOR(FUCHSIA, 255, 0, 255, 0);
+        CRGBA_STANDARD_COLOR(FUCHSIA_LIGHT, 255, 127, 255, 0);
+        CRGBA_STANDARD_COLOR(BROWN, 165, 42, 42, 0);
+        CRGBA_STANDARD_COLOR(MAGENTA, 229, 9, 127, 0);
+
+        static constexpr 
     };
 }
