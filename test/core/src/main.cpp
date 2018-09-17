bbc6bc5352bd87b0d007e3888522d172fb912846
Ian A.B. King <iansens@hotmail.com>
2018-09-17 18:54:33 +0200

    Some vector2 unit tests

--- test/core/src/main.cpp
+++ test/core/src/main.cpp
@@ -1,10 +1,11 @@
 #include "gtest/gtest.h"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-    ::testing::InitGoogleTest(&argc, argv); 
+    testing::InitGoogleTest(&argc, argv); 
     RUN_ALL_TESTS();
-    int aux = 0;
-    std::cin >> aux;
-    return 0;
+
+    std::cout << "Press Enter to exit..." << std::endl;
+    return std::getchar();
 }
--- test/core/src/vector2.cpp
+++ test/core/src/vector2.cpp
@@ -1,16 +1,145 @@
 #include "gtest/gtest.h"
 #include "vector2.hpp"
 
-TEST (VectorAdd, VectorOperators)
+#include <random>
+#include <ctime>
+
+static const int OP_TEST_ITER = 99999;
+
+static std::mt19937 Rand_Generator(static_cast<int>(time(0)));
+
+static std::uniform_real_distribution<float> Rand_Dist_Float(
+    std::numeric_limits<float>::min(), std::numeric_limits<float>::max());
+
+static std::uniform_int_distribution<int> Rand_Dist_Int(
+    std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+
+using rvi::Vector2;
+
+static float GetRandomFloat()
 {
-	rvi::Vector2 v1(5, 10);
-    rvi::Vector2 v2(6, 12);
-    ASSERT_EQ((v1+v2), rvi::Vector2(11, 22));
+    return Rand_Dist_Float(Rand_Generator);
 }
 
-TEST (VectorSub, VectorOperators)
+static int GetRandomInt()
+{    
+    return Rand_Dist_Int(Rand_Generator);
+}
+
+TEST (VectorOperators, Add)
 {
-	rvi::Vector2 v1(7, 77);
-    rvi::Vector2 v2(1, -5);
-    ASSERT_EQ((v1+v2), rvi::Vector2(8, 72));
+    float acc_x = 0;
+    float acc_y = 0;
+
+    Vector2 result(0, 0);
+    for(int i = 0; i < OP_TEST_ITER; i++)
+    {
+        float x = GetRandomFloat();
+        float y = GetRandomFloat();
+
+        Vector2 vec(x, y);
+
+        result += vec;
+
+        acc_x += x;
+        acc_y += y;
+    }
+
+    ASSERT_EQ(result.X, acc_x);
+    ASSERT_EQ(result.Y, acc_y);
+}
+
+TEST (VectorOperators, Sub)
+{
+	float acc_x = 0;
+    float acc_y = 0;
+
+    Vector2 result(0, 0);
+    for(int i = 0; i < OP_TEST_ITER; i++)
+    {
+        float x = GetRandomFloat();
+        float y = GetRandomFloat();
+
+        Vector2 vec(x, y);
+
+        result -= vec;
+
+        acc_x -= x;
+        acc_y -= y;
+    }
+
+    ASSERT_EQ(result.X, acc_x);
+    ASSERT_EQ(result.Y, acc_y);
+}
+
+TEST (VectorOperators, Multiply)
+{
+	float acc_x = 0;
+    float acc_y = 0;
+
+    Vector2 result(0, 0);
+    for(int i = 0; i < OP_TEST_ITER; i++)
+    {
+        float x = GetRandomFloat();
+        float y = GetRandomFloat();
+
+        Vector2 vec(x, y);
+
+        result *= vec;
+
+        acc_x *= x;
+        acc_y *= y;
+    }
+
+    ASSERT_EQ(result.X, acc_x);
+    ASSERT_EQ(result.Y, acc_y);
+}
+
+TEST (VectorOperators, Divide)
+{
+	float acc_x = 0;
+    float acc_y = 0;
+
+    Vector2 result(0, 0);
+    for(int i = 0; i < OP_TEST_ITER; i++)
+    {
+        float x = GetRandomFloat();
+        float y = GetRandomFloat();
+
+        Vector2 vec(x, y);
+
+        result /= vec;
+
+        acc_x /= x;
+        acc_y /= y;
+    }
+
+    ASSERT_EQ(result.X, acc_x);
+    ASSERT_EQ(result.Y, acc_y);
+}
+
+TEST (VectorOperators, Equality)
+{
+    for(int i = 0; i < OP_TEST_ITER; i++)
+    {
+        Vector2 a(GetRandomFloat(), GetRandomFloat());
+        Vector2 b(a.X, a.Y);
+        Vector2 c(GetRandomFloat(), GetRandomFloat());
+
+        ASSERT_TRUE(a == b);
+        ASSERT_FALSE(a == c);
+    }
+}
+
+TEST (VectorOperators, Inequality)
+{
+    for(int i = 0; i < OP_TEST_ITER; i++)
+    {
+        Vector2 a(GetRandomFloat(), GetRandomFloat());
+        Vector2 b(a.X, a.Y);
+        Vector2 c(GetRandomFloat(), GetRandomFloat());
+
+        ASSERT_FALSE(a != b);
+        ASSERT_TRUE(a != c);
+    }
 }
