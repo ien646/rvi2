ef813f415d0d098716d262aa62861441eb1b7f09
Ian A.B. King <iansens@hotmail.com>
2018-09-17 18:54:21 +0200

    roomba

--- RVI2.Core/src/instruction_generator.cpp
+++ RVI2.Core/src/instruction_generator.cpp
@@ -2,7 +2,6 @@
 
 #define CAPTURE_RVALUEREF(x) &x
 #define NOCAPTURE
-#define IGEN_LAMB_ARGS (ClientContext& c)
 
 typedef rvi::DefinitionInstruction INSTRUCTION;
 
@@ -12,79 +11,79 @@ namespace rvi
 {
     INSTRUCTION InstructionGenerator::DrawLine(Vector2 from, Vector2 to)
     {
-        return[=]IGEN_LAMB_ARGS
+        return[=](ClientContext& c)
         { c.DrawLine(from, to); };
     }
 
     INSTRUCTION InstructionGenerator::DrawLine(Vector2 from, ColorRGBA fromColor, Vector2 to, ColorRGBA toColor)
     {
-        return[=]IGEN_LAMB_ARGS
+        return[=](ClientContext& c)
         { c.DrawLine(from, fromColor, to, toColor); };
     }
 
     INSTRUCTION InstructionGenerator::DrawLine(Vertex from, Vertex to)
     {
-        return[=]IGEN_LAMB_ARGS
+        return[=](ClientContext& c)
         { c.DrawLine(from, to); };
     }
 
     INSTRUCTION InstructionGenerator::SelectFrame(const string& name)
     {
-        return[=]IGEN_LAMB_ARGS
+        return[=](ClientContext& c)
         { c.SelectFrame(name); };
     }
 
     INSTRUCTION InstructionGenerator::SelectFrame(string&& name)
     {
-        return[&]IGEN_LAMB_ARGS
+        return[&](ClientContext& c)
         { c.SelectFrame(std::move(name)); };
     }
 
     INSTRUCTION InstructionGenerator::ReleaseFrame()
     {
-        return[NOCAPTURE]IGEN_LAMB_ARGS
+        return[NOCAPTURE](ClientContext& c)
         { c.ReleaseFrame(); };
     }
 
     INSTRUCTION InstructionGenerator::SetCurrentColor(ColorRGBA color)
     {
-        return[=]IGEN_LAMB_ARGS noexcept
+        return[=](ClientContext& c) noexcept
         { c.SetCurrentColor(color); };
     }
 
     INSTRUCTION InstructionGenerator::SetCurrentTransform(const Transform2& tform)
     {
-        return[=]IGEN_LAMB_ARGS noexcept
+        return[=](ClientContext& c) noexcept
         { c.SetCurrentTransform(tform); };
     }
 
     INSTRUCTION InstructionGenerator::SetCurrentTransform(Transform2&& tform)
     {
-        return[&]IGEN_LAMB_ARGS noexcept
+        return[&](ClientContext& c) noexcept
         { c.SetCurrentTransform(std::move(tform)); };
     }
 
     INSTRUCTION InstructionGenerator::AddDefinition(const string& name, const Definition& instruction)
     {
-        return[name, instruction]IGEN_LAMB_ARGS
+        return[name, instruction](ClientContext& c)
         { c.AddDefinition(name,instruction); };
     }
 
     INSTRUCTION InstructionGenerator::AddDefinition(const string& name, Definition&& instruction)
     {
-        return[name, CAPTURE_RVALUEREF(instruction)]IGEN_LAMB_ARGS
+        return[name, CAPTURE_RVALUEREF(instruction)](ClientContext& c)
         { c.AddDefinition(name, std::move(instruction)); };
     }
 
     INSTRUCTION InstructionGenerator::AddDefinition(string&& name, const Definition& instruction)
     {
-        return[CAPTURE_RVALUEREF(name), instruction]IGEN_LAMB_ARGS
+        return[CAPTURE_RVALUEREF(name), instruction](ClientContext& c)
         { c.AddDefinition(std::move(name),instruction); };
     }
 
     INSTRUCTION InstructionGenerator::AddDefinition(string&& name, Definition&& instruction)
     {
-        return[CAPTURE_RVALUEREF(name), CAPTURE_RVALUEREF(instruction)]IGEN_LAMB_ARGS
+        return[CAPTURE_RVALUEREF(name), CAPTURE_RVALUEREF(instruction)](ClientContext& c)
         { c.AddDefinition(std::move(name), std::move(instruction)); };
     }
 }
