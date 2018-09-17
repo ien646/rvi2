2dbb04c814289132930c31e3efb3020979b66cca
Ian A.B. King <iansens@hotmail.com>
2018-09-17 18:54:26 +0200

    update build scripts

--- build.bat
+++ build.bat
@@ -1,4 +1,15 @@
+rmdir build /s /q
 md build
 cd build
 cmake ..
 cmake --build .
+
+move output ..\__temp
+cd ..
+rmdir build /s /q
+md build
+cd build
+move ..\__temp output
+
+echo "FINISHED!"
+pause
--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
+rm build -r
 mkdir build
 cd build
 cmake ..
 cmake --build .
+
+mv output ../__temp
+cd ..
+rm build -r
+mkdir build
+cd build
+mv ../__temp output
+
+echo "PRESS ANY KEY TO EXIT..."
+read -n 1
