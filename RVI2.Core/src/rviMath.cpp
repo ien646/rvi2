c4696ab2f82cebca098666ba00e66df633035036
Ian A.B. King <iansens@hotmail.com>
2018-09-17 18:54:11 +0200

    Some math methods

---
+++ RVI2.Core/src/rviMath.cpp
@@ -0,0 +1,25 @@
+#include "rviMath.h"
+
+namespace rvi
+{
+	float Math::Deg2Rad(float angleDeg)
+	{
+		return angleDeg * PI / 180;
+	}
+
+	float Math::Rad2Deg(float angleRad)
+	{
+		return angleRad * 180 / PI;
+	}
+
+	Math::Matrix2x2 Math::Get2DRotationMatrix(float angle)
+	{
+		float angleRadians = Deg2Rad(angle);
+
+		return Matrix2x2
+		{
+			std::cos(angleRadians), -std::sin(angleRadians),
+			std::sin(angleRadians),  std::cos(angleRadians)
+		};
+	}
+}
---
+++ RVI2.Core/src/rviMath.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "rviTypes.h"
+#include <array>
+#include <cmath>
+
+namespace rvi
+{
+	namespace Math
+	{
+		typedef std::array<float, 4> Matrix2x2;
+
+		const float PI = 3.141592653589793238F;
+
+		extern float Deg2Rad(float angleDeg);
+
+		extern float Rad2Deg(float angleRad);
+
+		extern Matrix2x2 Get2DRotationMatrix(float angle);
+	}
+}
