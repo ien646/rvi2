4076e120d3f50a1d515c013daad0966370e16d1a
Ian A.B. King <iansens@hotmail.com>
2018-09-20 23:12:33 +0200

    meh

---
+++ test/core/src/test_line.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "line.hpp"
+#include "random_gen.hpp"
+#include "line.hpp"
+
+using rvi::Line;
+using rvi::Vector2;
+
+TEST(Line, ApplyOffset)
+{
+	//Needs vertex tests first...
+}
