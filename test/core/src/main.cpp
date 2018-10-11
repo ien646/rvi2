e5301950a6b63ad32bf369ec2135c062606fa722
Ian A.B. King <iansens@hotmail.com>
2018-10-11 23:38:39 +0200

    test program now returns value

--- test/core/src/main.cpp
+++ test/core/src/main.cpp
@@ -6,8 +6,5 @@ int main(int argc, char **argv)
 {    
     testing::InitGoogleTest(&argc, argv); 
     testing::InitGoogleMock(&argc, argv);
-    RUN_ALL_TESTS();    
-
-    std::cout << "Press Enter to exit..." << std::endl;
-    return std::getchar();    
+    return RUN_ALL_TESTS();  
 }
