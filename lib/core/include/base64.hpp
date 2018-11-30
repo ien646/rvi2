b55e6cdc578d54e03dab9ed8897033bf50f30e0f
Ian A.B. King <iansens@hotmail.com>
2018-11-30 21:18:05 +0100

    WIP doxygen documentation

--- lib/core/include/base64.hpp
+++ lib/core/include/base64.hpp
@@ -5,6 +5,9 @@
 
 namespace rvi
 {
+    /**
+     * @brief Support for encoding/decoding base64 data
+     */
     class base64
     {
     private:
@@ -19,7 +22,20 @@ namespace rvi
             ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
             -1,-1,-1 };
     public:
+        /**
+         * @brief Encode a binary stream into a base64 string-stream
+         * 
+         * @param in Input binary stream
+         * @param out Output base64 encoded string-stream
+         */
         void encode(std::istream& in, std::ostringstream& out);
+
+        /**
+         * @brief Decode a base64 string-stream into a binary stream
+         * 
+         * @param in Input base64 encoded string-stream
+         * @param out Output binary stream
+         */
         void decode(std::istringstream& in, std::ostream& out);
     };
 }
--- lib/core/include/color_rgba.hpp
+++ lib/core/include/color_rgba.hpp
@@ -6,15 +6,34 @@
 
 namespace rvi
 {
+    /**
+     * @brief RGBA color data structure
+     */
     struct color_rgba
     {
+        ///@brief Red channel value
         u8 r = 0;
+
+        ///@brief Green channel value
         u8 g = 0;
+
+        ///@brief Blue channel value
         u8 b = 0;
+
+        ///@brief Alpha channel value
         u8 a = 0;
 
+        ///@brief Construct a new color_rgba object [rgba = (0,0,0,0)]
         constexpr color_rgba() noexcept { };
 
+        /**
+         * @brief Construct a new color_rgba object
+         * 
+         * @param r Red channel value
+         * @param g Green channel value
+         * @param b Blue channel value
+         * @param a Alpha channel value
+         */
         constexpr color_rgba(u8 r, u8 g, u8 b, u8 a) noexcept
         {
             this->r = r;
@@ -23,14 +42,22 @@ namespace rvi
             this->a = a;
         }
 
+        ///@brief Pack channel data into a 4-byte scalar
         u32 rgba()
         {
             return (r << 24) | (g << 16) | (b << 8) | (a);
         }
 
+        /**
+         * @brief Construct a new color_rgba from a 4-byte scalar,
+         * where each byte represents a channel value
+         * 
+         * @param rgba 4-byte scalar rgba value
+         */
         static color_rgba from_rgba(uint32_t rgba);
 
         bool operator==(color_rgba other) const noexcept;
+
         bool operator!=(color_rgba other) const noexcept;
 
         std::string to_string() const;
