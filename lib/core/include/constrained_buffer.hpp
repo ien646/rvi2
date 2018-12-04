9a54c2bd6c9ca3cd05d30e79d473b724754f5e2a
Ian A.B. King <iansens@hotmail.com>
2018-12-04 19:18:53 +0100

    Constrained buffer documentation

--- lib/core/include/constrained_buffer.hpp
+++ lib/core/include/constrained_buffer.hpp
@@ -5,6 +5,13 @@
 
 namespace rvi
 {
+    /**
+     * @brief Size constrained buffer
+     * 
+     * @tparam T Buffer element type
+     * @tparam max_sz Maximum number of elements allowed
+     * @tparam std::enable_if_t<std::is_scalar_v<T>> Only scalar types allowed
+     */
     template<typename T, size_t max_sz, typename = std::enable_if_t<std::is_scalar_v<T>>>
     class constrained_buffer
     {
@@ -13,13 +20,22 @@ namespace rvi
 
     public:
         
+        ///Default constructor
         constexpr constrained_buffer() { }
-       
+        
+        ///Maximum number of elements allowed 
         constexpr size_t max_size() const noexcept
         {
             return max_sz;
         }
 
+        /**
+         * @brief Push an element to the back of the buffer.
+         * If the buffer size after insertion is bigger than the
+         * stablished maximum size, the oldest element is removed.
+         * 
+         * @param val Element to append
+         */
         void push_back(const T& val)
         {
             buffer.push_back(val);
@@ -29,6 +45,13 @@ namespace rvi
             }
         }
 
+        /**
+         * @brief Push an element to the back of the buffer.
+         * If the buffer size after insertion is bigger than the
+         * stablished maximum size, the oldest element is removed.
+         * 
+         * @param val Element to append
+         */
         void push_back(T&& val)
         {
             buffer.push_back(std::move(val));
@@ -38,16 +61,19 @@ namespace rvi
             }
         }
 
+        ///Remove all elements from the buffer
         void clear() noexcept
         {
             buffer.clear();
         }
 
+        ///Buffer begin iterator
         auto begin() noexcept
         {
             return buffer.begin();
         }
 
+        ///Buffer end iterator
         auto end() noexcept
         {
             return buffer.end();
--- s_gen_docs.sh
+++ s_gen_docs.sh
@@ -1,3 +1,4 @@
 rm latex -r
 rm html -r
 doxygen Doxyfile
+mv html/ docs
