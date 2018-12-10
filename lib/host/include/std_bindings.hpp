fccacb55e34e272d9d0874b604ae614f956a8dc8
Ian A.B. King <iansens@hotmail.com>
2018-12-10 18:28:20 +0100

    std_bindings text printing refactor

--- lib/host/include/std_bindings.hpp
+++ lib/host/include/std_bindings.hpp
@@ -4,7 +4,41 @@
 
 namespace rvi::std_bindings
 {
+    static const float DEF_FONT_SZ = 0.03F;
+    static const float DEF_FONT_HSEP = 0.075F;
+    static const float DEF_FONT_MARGIN = 0.01F;
+    static const char  DEF_FONT_WRAP_SEP_CH = ' ';
+
     extern void print(client_instance& c_inst, const arglist_t& args);
+    extern void printw(client_instance& c_inst, const arglist_t& args);
+    extern void print_ex(client_instance& c_inst, const arglist_t& args);
+    extern void printw_ex(client_instance& c_inst, const arglist_t& args);
+    extern void printc(client_instance& c_inst, const arglist_t& args);
+    extern void printcw(client_instance& c_inst, const arglist_t& args);
+    
+    extern void _print(
+        client_instance& c_inst,
+        const std::string& calling_frame,
+        const std::string& text,
+        float font_sz_h,
+        float font_sz_v,
+        float font_sep_h,
+        float font_sep_v,
+        float font_margin_h,
+        float font_margin_v);
+
+    extern void _printw(
+        client_instance& c_inst,
+        const std::string& calling_frame,
+        const std::string& text,
+        float font_sz_h,
+        float font_sz_v,
+        float font_sep_h,
+        float font_sep_v,
+        float font_margin_h,
+        float font_margin_v,
+        char wsep_char);
+
     extern void box_border(client_instance& c_inst, const arglist_t& args);
 
     extern void init_std_bindings(client_instance& c_inst);
--- lib/host/src/std_bindings.cpp
+++ lib/host/src/std_bindings.cpp
@@ -15,32 +15,88 @@ namespace rvi
 
     void std_bindings::print(client_instance& c_inst, const arglist_t& args)
     {
-        expect_argc(args, 5 + 1);
+        expect_argc(args, 1 + 1);
+        const std::string& text = args[0];
+        const std::string& cframe = args[1];
 
-        std::string text = args[0];
-        float fontsz_x = std::stof(args[1]);
-        float fontsz_y = std::stof(args[2]);
-        float fontsep_x = std::stof(args[3]);
-        float fontsep_y = std::stof(args[4]);
-        float margin_x = std::stof(args[5]);
-        float margin_y = std::stof(args[6]);
-        std::string calling_frame = args[7];
+        std_bindings::_print(
+            c_inst, 
+            cframe,
+            text,
+            DEF_FONT_SZ, 
+            DEF_FONT_SZ,
+            DEF_FONT_HSEP,
+            0.0F,
+            DEF_FONT_MARGIN,
+            DEF_FONT_MARGIN);
+    }
 
+    void std_bindings::printw(client_instance& c_inst, const arglist_t& args)
+    {
+        expect_argc(args, 1 + 1);
+        const std::string& text = args[0];
+        const std::string& cframe = args[1];
+
+        std_bindings::_printw(
+            c_inst, 
+            cframe,
+            text,
+            DEF_FONT_SZ, 
+            DEF_FONT_SZ,
+            DEF_FONT_HSEP,
+            0.0F,
+            DEF_FONT_MARGIN,
+            DEF_FONT_MARGIN,
+            DEF_FONT_WRAP_SEP_CH);
+    }
+
+    void std_bindings::print_ex(client_instance& c_inst, const arglist_t& args)
+    {
+
+    }
+
+    void std_bindings::printw_ex(client_instance& c_inst, const arglist_t& args)
+    {
+
+    }
+
+    void std_bindings::printc(client_instance& c_inst, const arglist_t& args)
+    {
+
+    }
+
+    void std_bindings::printcw(client_instance& c_inst, const arglist_t& args)
+    {
+        
+    }
+
+    extern void std_bindings::_print(
+        client_instance& c_inst,
+        const std::string& calling_frame,
+        const std::string& text,
+        float font_sz_h,
+        float font_sz_v,
+        float font_sep_h,
+        float font_sep_v,
+        float font_margin_h,
+        float font_margin_v)
+    {
         std::string buff = { 0, 0, 0 };
-        rvi::vector2 curPos(margin_x, margin_y);
-        int rid = std::rand();
-
         auto& ctx = c_inst.context;
         frame* save_fptr = ctx.selected_frame();
         frame* calling_fptr = ctx.find_frame(calling_frame);
-        ctx.select_frame(calling_fptr);
 
+        vector2 offset(font_margin_h, font_margin_v);
+
+        ctx.select_frame(calling_fptr);
+        int idx = 0;
         for (auto& ch : text)
         {
             buff[0] = buff[1];
             buff[1] = buff[2];
             buff[2] = ch;
-            std::string defn = "$ss";
+
+            std::string defn;
             if (buff[0] == '$')
             {
                 defn = buff;
@@ -54,28 +110,53 @@ namespace rvi
                 defn = std::string(1, ch);
             }
             
-            ctx.select_frame(std::to_string(rid++) + (ch));
-            ctx.set_position(curPos);
-            ctx.set_scale(rvi::vector2(fontsz_x, fontsz_y));
-            ctx.set_transform_scale_abs(true);
-            c_inst.exec_definition(defn);
+            ctx.select_frame("char_" + std::to_string(idx));
+            {
+                ctx.set_scale(vector2(font_sz_h, font_sz_v));
+                ctx.set_transform_scale_abs(true);
+                ctx.set_position(offset);
+                c_inst.exec_definition(defn);
+            }
             ctx.release_frame();
 
-            curPos.offset_in_place(rvi::vector2(fontsz_x + fontsep_x, fontsep_y));
+            offset += vector2(font_sz_h + font_sep_h, font_sz_v + font_sep_v);
+            ++idx;
         }
-
         ctx.select_frame(save_fptr);
+    }
+
+    extern void std_bindings::_printw(
+        client_instance& c_inst,
+        const std::string& calling_frame,
+        const std::string& text,
+        float font_sz_h,
+        float font_sz_v,
+        float font_sep_h,
+        float font_sep_v,
+        float font_margin_h,
+        float font_margin_v,
+        char wsep_char)
+    {
+        // ...
     }
 
     void std_bindings::box_border(client_instance& c_inst, const arglist_t& args)
     {
+        expect_argc(args, 0 + 1);
+
+        auto& calling_frame = args[0];
         auto& ctx = c_inst.context;
+
+        frame* save_fptr = ctx.selected_frame();
+        frame* calling_frame = ctx.find_frame(args[0]);
+
+        ctx.select_frame(calling_frame);
         ctx.select_frame("__STD_BOX_BORDER");
         ctx.draw_line(vector2(0, 0), vector2(1, 0)); //  -
         ctx.draw_line(vector2(0, 1), vector2(1, 1)); //  =
         ctx.draw_line(vector2(0, 0), vector2(0, 1)); // |=
         ctx.draw_line(vector2(1, 0), vector2(1, 1)); // |=|
-        ctx.release_frame();
+        ctx.select_frame(save_fptr);
     }
 
     void std_bindings::init_std_bindings(client_instance& c_inst)
