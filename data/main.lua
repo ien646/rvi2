29b881be1899419f0918876d598e6fe4d13ba632
Ian A.B. King <iansens@hotmail.com>
2019-02-03 01:44:25 +0100

    rvi lua-side easy colors

--- data/main.lua
+++ data/main.lua
@@ -1,10 +1,11 @@
-include("rvi/rvi_font_base.lua");
+include("rvi/rvi_font.lua");
+include("rvi/rvi_color.lua");
 
 select_frame("hello_world");
 
 	set_position(0.10, 0.45);
 	set_scale(0.75, 0.10);
-	set_color(255, 0, 255, 255);
+	rset_color(rvi_color["pink"]);
     
     rprint("Hello Friend");
 	
---
+++ data/rvi/rvi_color.lua
@@ -0,0 +1,19 @@
+rvi_color = {
+	black 	= { 0, 		0, 		0, 		255 },
+	gray	= { 127,	127,	127, 	255 },
+	grey	= { 127,	127,	127, 	255 },
+	white 	= { 255,  	255, 	255, 	255 },
+	red		= { 255, 	0,		0,		255 },
+	green	= { 0,		255,	0,		255 },
+	blue	= { 0,		0,		255,	255 },
+	yellow	= { 255,	255,	0,		255 },
+	magenta = { 255,	0,		255,	255 },
+	fuchsia	= { 255,	127,	255,	255 },
+	orange 	= { 255,	127,	0,		255 },
+	brown	= { 170, 	119, 	54,		255 },
+	pink	= { 255,	100,	200,	255 }
+};
+
+function rset_color(color)
+	set_color(color[1], color[2], color[3], color[4]);
+end
