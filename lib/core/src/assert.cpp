f2537a0bad38434d1726c411e31f8bff510cb46a
Ian A.B. King <iansens@hotmail.com>
2019-02-12 01:29:03 +0100

    r_assert now outputs message to stderr

--- lib/core/src/assert.cpp
+++ lib/core/src/assert.cpp
@@ -1,11 +1,13 @@
 #include <rvi/assert.hpp>
 
 #include <stdexcept>
+#include <iostream>
 
 namespace rvi
 {
     void r_assert(bool cond, const std::string& msg)
     {
+        std::cerr << msg << std::endl;
         if(!cond)        
             throw std::logic_error(msg);
     }
